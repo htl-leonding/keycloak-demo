bd4afea2de5a78fd6c40c93ff3a401f1d0b7da84
htl-leonding <t.stuetz@htl-leonding.ac.at>
2021-04-09 13:32:18 +0200

    backend complete

--- src/main/java/at/htl/GreetingResource.java
+++ src/main/java/at/htl/GreetingResource.java
@@ -1,9 +1,16 @@
 package at.htl;
 
+import io.quarkus.security.Authenticated;
+import io.quarkus.security.runtime.interceptor.check.SecurityCheck;
+import io.smallrye.config.SecretKeysConfigSourceInterceptor;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import java.util.List;
 
 @Path("/hello-resteasy")
 public class GreetingResource {
@@ -13,4 +20,19 @@ public class GreetingResource {
     public String hello() {
         return "Hello RESTEasy";
     }
+
+    @GET
+    @Path("user")
+    @Authenticated
+    public List<String> loggedInUser() {
+        return List.of("a", "b", "c");
+    }
+
+   @GET
+    @Path("user/me")
+    @Authenticated
+    public String me(@Context SecurityContext sc) {
+        return sc.getUserPrincipal().getName();
+    }
+
 }
